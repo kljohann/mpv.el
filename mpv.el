9ff4d5d80b762dabbea3c29a378147f7e8156437
Johann Klähn <kljohann@gmail.com>
2015-02-16 16:26:57 +0100

    fix mpv-insert-playback-position
    Turns out I've been using lexical-binding after all.

--- mpv.el
+++ mpv.el
@@ -1,10 +1,10 @@
-;;; mpv.el --- control mpv for easy note-taking
+;;; mpv.el --- control mpv for easy note-taking  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2014  Johann Klähn
 
 ;; Author: Johann Klähn <kljohann@gmail.com>
 ;; Keywords: tools, multimedia
-;; Package-Requires: ((cl-lib "0.5") (json "1.3") (names "0.5.4") (org "8.0"))
+;; Package-Requires: ((cl-lib "0.5") (emacs "24") (json "1.3") (names "0.5.4") (org "8.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
